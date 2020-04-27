38da34376c51dad78e103bbec076a582d2dd1e3e
davidesiciliano <37239185+davidesiciliano@users.noreply.github.com>
2020-04-27 10:39:30 +0200

    Query 3 funzionante

--- src/main/java/start/Start.java
+++ src/main/java/start/Start.java
@@ -5,13 +5,10 @@ import database.Database;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 import utils.LogUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -35,18 +32,28 @@ public class Start {
         Database.getDB().constructSchema();
         Database.getDB().loadDataset(filePath);
 
+        LocalDateTime init = LocalDateTime.now();
+        System.out.println(">>>>>>INIT TIME: " + init);
+
         final Dataset<Row> completeDataset = Database.getDB().getDataset();
+
+        System.out.println(">>>>> DIM INIZIALE: " + completeDataset.count());
 
         //QUERY 1: Number of lethal accidents per week throughout the entire dataset
         //QUERY 2: Number of accidents and percentage of number of deaths per contributing factor in the dataset
         //QUERY 3: Number of accidents and average number of lethal accidents per week per borough
 
+        LocalDateTime afterReading = LocalDateTime.now();
+        Duration readingTime = Duration.between(init, afterReading);
+        System.out.println(">>>>>>READING TIME: " + readingTime);
+
         final Dataset<Row> cleanDataset = completeDataset
                 .where(col(Constants.BOROUGH).isNotNull()
-                        .and(col(Constants.DATE).isNotNull()));
+                        .and(col(Constants.DATE).isNotNull()))
+                .cache();
 
         final Dataset<Row> d1 = cleanDataset
-                .withColumn(Constants.WEEK, date_format(col(Constants.DATE), "w"))
+                .withColumn(Constants.WEEK, weekofyear(col(Constants.DATE)))
                 .withColumn(Constants.YEAR, date_format(col(Constants.DATE), "yyyy"))
                 .withColumn(Constants.NUMBER_INJURED, col(Constants.NUMBER_OF_PERSONS_INJURED)
                         .plus(col(Constants.NUMBER_OF_PEDESTRIANS_INJURED))
@@ -63,10 +70,8 @@ public class Start {
                         Constants.NUMBER_KILLED);
         d1.show();
 
-        // con dataset corretto parte da 2014
-        // mentre con dataset sbagliato parte da 2012
         // --> i risultati comunque sono sbagliati rispetto alle foto
-        final Dataset<Row> d2 = d1
+        final Dataset<Row> query3 = d1
                 .groupBy(Constants.BOROUGH, Constants.YEAR, Constants.WEEK)
                 .agg(count("*").as(Constants.NUMBER_ACCIDENTS),
                         count(when(col(Constants.NUMBER_KILLED).$greater(0), true)).as(Constants.NUMBER_LETHAL_ACCIDENTS),
@@ -74,7 +79,14 @@ public class Start {
                         sum(col(Constants.NUMBER_KILLED)).as("sum(NumberKilled)"))
                 .withColumn("avgLethal", col(Constants.NUMBER_LETHAL_ACCIDENTS).divide(col(Constants.NUMBER_ACCIDENTS)))
                 .sort(Constants.YEAR, Constants.WEEK, Constants.BOROUGH);
-        d2.show(100);
+        query3.show(100);
+
+        LocalDateTime end = LocalDateTime.now();
+        System.out.println(">>>>>>END TIME: " + end);
+        Duration executionTime = Duration.between(afterReading, end);
+        System.out.println(">>>>>>EXECUTION TIME: " + executionTime);
+        Duration totalTime = Duration.between(init, end);
+        System.out.println(">>>>>>TOTAL TIME: " + totalTime);
 
         spark.close();
     }
