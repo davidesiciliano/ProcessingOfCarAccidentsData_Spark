9870b3d559d567b9b01290bc829658896c7241d9
davidesiciliano <37239185+davidesiciliano@users.noreply.github.com>
2020-04-29 17:40:30 +0200

    Refactor

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -10,16 +10,25 @@ import org.apache.spark.sql.types.StructType;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.apache.spark.sql.functions.*;
+
 public class Database {
     private static Database databaseInstance;
     private final SparkSession spark;
     private StructType mySchema;
     private Dataset<Row> dataset;
 
+    private Dataset<Row> query1;
+    private Dataset<Row> query2;
+    private Dataset<Row> query3;
+
     private Database(SparkSession spark) {
         this.spark = spark;
         this.mySchema = null;
         this.dataset = null;
+        this.query1 = null;
+        this.query2 = null;
+        this.query3 = null;
     }
 
     public static void initializeDatabase(SparkSession spark) {
@@ -35,7 +44,7 @@ public class Database {
         return databaseInstance;
     }
 
-    public void constructSchema() {
+    private void constructSchema() {
         if (mySchema != null)
             System.err.println("ERRORE"); //TODO: add exception
         // DATE, TIME, BOROUGH, ZIP CODE, LATITUDE, LONGITUDE, LOCATION, ON STREET NAME,
@@ -79,22 +88,94 @@ public class Database {
     }
 
     public void loadDataset(String filePath) {
-        if (this.dataset == null)
-            this.dataset = spark
-                    .read()
-                    .option("header", "true")
-                    .option("dateFormat", "MM/dd/yyyy")
-                    .option("timestampFormat", "hh:mm")
-                    .option("delimiter", ",")
-                    //.option("inferSchema", "true")
-                    .schema(this.mySchema)
-                    .csv(filePath + "data/NYPD_Motor_Vehicle_Collisions.csv");
-                    //.csv(filePath + "data/DatasetProva.csv");
-        else
+        if (this.dataset != null)
             System.err.println("Dataset already loaded");
+        this.constructSchema();
+        final Dataset<Row> completeDataset = spark
+                .read()
+                .option("header", "true")
+                .option("dateFormat", "MM/dd/yyyy")
+                .option("timestampFormat", "hh:mm")
+                .option("delimiter", ",")
+                //.option("inferSchema", "true")
+                .schema(this.mySchema)
+                .csv(filePath + "data/NYPD_Motor_Vehicle_Collisions.csv");
+                //.csv(filePath + "data/DatasetProva.csv");
+        this.dataset = completeDataset //clean dataset without Null values and useless columns
+                .where(col(Constants.BOROUGH).isNotNull()
+                        .and(col(Constants.DATE).isNotNull()))
+                .select(col(Constants.DATE),
+                        col(Constants.BOROUGH),
+                        col(Constants.NUMBER_OF_PERSONS_INJURED),
+                        col(Constants.NUMBER_OF_PERSONS_KILLED),
+                        col(Constants.NUMBER_OF_PEDESTRIANS_INJURED),
+                        col(Constants.NUMBER_OF_PEDESTRIANS_KILLED),
+                        col(Constants.NUMBER_OF_CYCLIST_INJURED),
+                        col(Constants.NUMBER_OF_CYCLIST_KILLED),
+                        col(Constants.NUMBER_OF_MOTORIST_INJURED),
+                        col(Constants.NUMBER_OF_MOTORIST_KILLED),
+                        col(Constants.CONTRIBUTING_FACTOR_VEHICLE_1),
+                        col(Constants.CONTRIBUTING_FACTOR_VEHICLE_2),
+                        col(Constants.CONTRIBUTING_FACTOR_VEHICLE_3),
+                        col(Constants.CONTRIBUTING_FACTOR_VEHICLE_4),
+                        col(Constants.CONTRIBUTING_FACTOR_VEHICLE_5),
+                        col(Constants.UNIQUE_KEY))
+                .cache();
     }
 
-    public Dataset<Row> getDataset() {
-        return dataset;
+    public Dataset<Row> executeQuery1() {
+        //QUERY 1: Number of lethal accidents per week throughout the entire dataset
+        if (this.query3 == null)
+            this.executeQuery3();
+        if (this.query1 != null)
+            return this.query1;
+
+        this.query1 = this.query3
+                .groupBy(Constants.YEAR, Constants.WEEK)
+                .agg(sum(col(Constants.NUMBER_LETHAL_ACCIDENTS)).as(Constants.SUM_NUMBER_LETHAL_ACCIDENTS))
+                .select(Constants.YEAR, Constants.WEEK, Constants.SUM_NUMBER_LETHAL_ACCIDENTS)
+                .sort(Constants.YEAR, Constants.WEEK);
+
+        return this.query1;
+    }
+
+    public Dataset<Row> executeQuery2() {
+        //QUERY 2: Number of accidents and percentage of number of deaths per contributing factor in the dataset
+        return null;
+    }
+
+    public Dataset<Row> executeQuery3() {
+        //QUERY 3: Number of accidents and average number of lethal accidents per week per borough
+        if (this.query3 != null)
+            return this.query3;
+
+        final Dataset<Row> d1 = this.dataset
+                .withColumn(Constants.WEEK, weekofyear(col(Constants.DATE)))
+                .withColumn(Constants.YEAR, date_format(col(Constants.DATE), "yyyy"))
+                .withColumn(Constants.NUMBER_INJURED, col(Constants.NUMBER_OF_PERSONS_INJURED)
+                        .plus(col(Constants.NUMBER_OF_PEDESTRIANS_INJURED))
+                        .plus(col(Constants.NUMBER_OF_CYCLIST_INJURED))
+                        .plus(col(Constants.NUMBER_OF_MOTORIST_INJURED)))
+                .withColumn(Constants.NUMBER_KILLED, col(Constants.NUMBER_OF_PERSONS_KILLED)
+                        .plus(col(Constants.NUMBER_OF_PEDESTRIANS_KILLED))
+                        .plus(col(Constants.NUMBER_OF_CYCLIST_KILLED))
+                        .plus(col(Constants.NUMBER_OF_MOTORIST_KILLED)))
+                .select(Constants.BOROUGH,
+                        Constants.YEAR,
+                        Constants.WEEK,
+                        Constants.NUMBER_INJURED,
+                        Constants.NUMBER_KILLED)
+                .cache();
+        this.query3 = d1
+                .groupBy(Constants.BOROUGH, Constants.YEAR, Constants.WEEK)
+                .agg(count("*").as(Constants.NUMBER_ACCIDENTS),
+                        count(when(col(Constants.NUMBER_KILLED).$greater(0), true)).as(Constants.NUMBER_LETHAL_ACCIDENTS),
+                        sum(col(Constants.NUMBER_INJURED)).as(Constants.SUM_NUMBER_INJURED),
+                        sum(col(Constants.NUMBER_KILLED)).as(Constants.SUM_NUMBER_KILLED))
+                .withColumn(Constants.AVERAGE_NUMBER_LETHAL_ACCIDENTS, col(Constants.NUMBER_LETHAL_ACCIDENTS).divide(col(Constants.NUMBER_ACCIDENTS)))
+                .sort(Constants.YEAR, Constants.WEEK, Constants.BOROUGH)
+                .cache();
+
+        return this.query3;
     }
 }
--- src/main/java/start/Start.java
+++ src/main/java/start/Start.java
@@ -1,6 +1,5 @@
 package start;
 
-import database.Constants;
 import database.Database;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -9,8 +8,6 @@ import utils.LogUtils;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-
-import static org.apache.spark.sql.functions.*;
 
 public class Start {
     public static void main(String[] args) {
@@ -28,79 +25,46 @@ public class Start {
                 .appName("CarAccidents")
                 .getOrCreate();
 
-        Database.initializeDatabase(spark);
-        Database.getDB().constructSchema();
-        Database.getDB().loadDataset(filePath);
-
+        //INIT TIME
         LocalDateTime init = LocalDateTime.now();
         System.out.println(">>>>>>INIT TIME: " + init);
 
-        final Dataset<Row> completeDataset = Database.getDB().getDataset();
+        Database.initializeDatabase(spark);
+        Database.getDB().loadDataset(filePath);
 
-        System.out.println(">>>>> DIM INIZIALE: " + completeDataset.count());
-
-        //QUERY 1: Number of lethal accidents per week throughout the entire dataset
-        //QUERY 2: Number of accidents and percentage of number of deaths per contributing factor in the dataset
-        //QUERY 3: Number of accidents and average number of lethal accidents per week per borough
-
+        //READING TIME
         LocalDateTime afterReading = LocalDateTime.now();
         Duration readingTime = Duration.between(init, afterReading);
         System.out.println(">>>>>>READING TIME: " + readingTime);
 
-        final Dataset<Row> cleanDataset = completeDataset
-                .where(col(Constants.BOROUGH).isNotNull()
-                        .and(col(Constants.DATE).isNotNull()));
-
-        final Dataset<Row> d1 = cleanDataset
-                .withColumn(Constants.WEEK, weekofyear(col(Constants.DATE)))
-                .withColumn(Constants.YEAR, date_format(col(Constants.DATE), "yyyy"))
-                .withColumn(Constants.NUMBER_INJURED, col(Constants.NUMBER_OF_PERSONS_INJURED)
-                        .plus(col(Constants.NUMBER_OF_PEDESTRIANS_INJURED))
-                        .plus(col(Constants.NUMBER_OF_CYCLIST_INJURED))
-                        .plus(col(Constants.NUMBER_OF_MOTORIST_INJURED)))
-                .withColumn(Constants.NUMBER_KILLED, col(Constants.NUMBER_OF_PERSONS_KILLED)
-                        .plus(col(Constants.NUMBER_OF_PEDESTRIANS_KILLED))
-                        .plus(col(Constants.NUMBER_OF_CYCLIST_KILLED))
-                        .plus(col(Constants.NUMBER_OF_MOTORIST_KILLED)))
-                .select(Constants.BOROUGH,
-                        Constants.YEAR,
-                        Constants.WEEK,
-                        Constants.NUMBER_INJURED,
-                        Constants.NUMBER_KILLED)
-                .cache();
-
-        LocalDateTime initQuery3 = LocalDateTime.now();
-        System.out.println(">>>>>>INIT TIME QUERY 3: " + initQuery3);
-        Duration cleaningTime = Duration.between(afterReading, initQuery3);
-        System.out.println(">>>>>>EXECUTION TIME CLEANING: " + cleaningTime);
-
-        final Dataset<Row> query3 = d1
-                .groupBy(Constants.BOROUGH, Constants.YEAR, Constants.WEEK)
-                .agg(count("*").as(Constants.NUMBER_ACCIDENTS),
-                        count(when(col(Constants.NUMBER_KILLED).$greater(0), true)).as(Constants.NUMBER_LETHAL_ACCIDENTS),
-                        sum(col(Constants.NUMBER_INJURED)).as(Constants.SUM_NUMBER_INJURED),
-                        sum(col(Constants.NUMBER_KILLED)).as(Constants.SUM_NUMBER_KILLED))
-                .withColumn(Constants.AVERAGE_NUMBER_LETHAL_ACCIDENTS, col(Constants.NUMBER_LETHAL_ACCIDENTS).divide(col(Constants.NUMBER_ACCIDENTS)))
-                .sort(Constants.YEAR, Constants.WEEK, Constants.BOROUGH);
+        final Dataset<Row> query3 = Database.getDB().executeQuery3();
         query3.show();
 
+        //QUERY 3 TIME
         LocalDateTime endQuery3 = LocalDateTime.now();
         System.out.println(">>>>>>END TIME QUERY 3: " + endQuery3);
-        Duration executionTimeQuery3 = Duration.between(initQuery3, endQuery3);
+        Duration executionTimeQuery3 = Duration.between(afterReading, endQuery3);
         System.out.println(">>>>>>EXECUTION TIME QUERY 3: " + executionTimeQuery3);
 
-        final Dataset<Row> query1 = query3
-                .groupBy(Constants.YEAR, Constants.WEEK)
-                .agg(sum(col(Constants.NUMBER_LETHAL_ACCIDENTS)).as(Constants.SUM_NUMBER_LETHAL_ACCIDENTS))
-                .select(Constants.YEAR, Constants.WEEK, Constants.SUM_NUMBER_LETHAL_ACCIDENTS)
-                .sort(Constants.YEAR, Constants.WEEK);
+        final Dataset<Row> query1 = Database.getDB().executeQuery1();
         query1.show();
 
+        //QUERY 1 TIME
         LocalDateTime endQuery1 = LocalDateTime.now();
         System.out.println(">>>>>>END TIME QUERY 1: " + endQuery1);
         Duration executionTimeQuery1 = Duration.between(endQuery3, endQuery1);
         System.out.println(">>>>>>EXECUTION TIME QUERY 1: " + executionTimeQuery1);
 
+        //final Dataset<Row> query2 = Database.getDB().executeQuery2();
+        //query2.show();
+
+        //QUERY 2 TIME
+        LocalDateTime endQuery2 = LocalDateTime.now();
+        System.out.println(">>>>>>END TIME QUERY 2: " + endQuery2);
+        Duration executionTimeQuery2 = Duration.between(endQuery1, endQuery2);
+        System.out.println(">>>>>>EXECUTION TIME QUERY 2: " + executionTimeQuery2);
+
+        //TOTAL TIME
         LocalDateTime end = LocalDateTime.now();
         Duration totalTime = Duration.between(init, end);
         System.out.println(">>>>>>TOTAL TIME: " + totalTime);
